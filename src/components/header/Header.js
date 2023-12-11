1ebd2fd054e0a93ae06bed8abbb25e51ccf51017
o6ez9na <kikikippplo@gmail.com>
2023-12-12 02:43:29 +0300

    header && test

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="Header">
+            Тут типа такой красивый значок какой нибудь будет типа да
+        </header>
+        );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/app/App.js';
+import Header from './components/header/Header.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>
 );
